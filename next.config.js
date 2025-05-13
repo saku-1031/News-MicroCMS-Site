a984c6bb8bb0afbc437763aece99e5ebf126d1fa
Saku Tsutomu <90943366+saku-1031@users.noreply.github.com>
2025-05-13 18:15:53 +0900

    next.config.jsを削除し、next.config.tsに環境変数設定を追加

--- next.config.js
+++
@@ -1,7 +0,0 @@
-require('dotenv').config();
-
-module.exports = {
-  env: {
-    API_KEY: process.env.API_KEY,
-  },
-}; 
--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,9 @@ const nextConfig: NextConfig = {
   images: {
     domains: ["images.microcms-assets.io"], // 画像のホスト名を追加
   },
+  env: {
+    API_KEY: process.env.API_KEY,
+  },
 };
 
 export default nextConfig;
